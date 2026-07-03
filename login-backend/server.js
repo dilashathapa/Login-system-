5b22d98ba819279b339cb0c2573c52ea0d4c5429
dilashathapa <dilashathapa15@gmail.com>
2026-07-03 16:12:12 +0545

    Database Connection

--- login-backend/server.js
+++ login-backend/server.js
@@ -1,22 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
-dotenv.config();
+dotenv.config(); // Load environment variables
+const PORT = process.env.PORT || 5000;// Get port from environment variables or use 5000 as default
+const app = express();// Create Express application
+const router = express.Router();
 
-const app = express();
+//===MIDDLEWARE===
+app.use(express.json());// Parse incoming JSON requests
+// Enable CORS for frontend application
+app.use(cors({
+    origin: "http://localhost:5173",// Frontend URL
+    credentials: true,// Allow cookies and credentials
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+}))
+app.use(cookieParser());// Parse cookies from incoming requests
 
 // Test Route
-app.get("/", (req, res) => {
-  res.send("Login Backend API is running...");
+router.get("/", (req, res) => {
+  res.send("Auth Route is working!");
 });
 
-const PORT = process.env.PORT || 5000;
+//===DATABASE CONNECTION===
+connectDB();
 
+// Start server and listen on specified port
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+export default router;
