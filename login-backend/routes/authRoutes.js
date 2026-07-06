91ce6c6ee79fec90e4e4858a2e1742fa5cf10573
dilashathapa <dilashathapa15@gmail.com>
2026-07-06 11:31:58 +0545

    Added authentication routes

--- login-backend/routes/authRoutes.js
+++ login-backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { login, logout, register, refreshToken } from "../controllers/authController.js";
 
 const router = express.Router();
+//===AUTH ROUTES===
+router.post("/register", register);// Route to register a new user
+router.post("/login", login);// Route to log in an existing user
+router.get("/refresh", refreshToken);// Route to generate a new access token using refresh token
+router.post("/logout", logout);// Route to log out user and clear refresh token cookie
 
-router.get("/", (req, res) => {
-    res.send("Auth Route is working!");
-});
-
-export default router;
+export default router;// Export router for use in the main application
